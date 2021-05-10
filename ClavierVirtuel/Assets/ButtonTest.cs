877309c51b72166b9206dd100508e19a314fb599
SYNTHEASYQUEST <75647141+SYNTHEASYQUEST@users.noreply.github.com>
2021-05-10 03:58:32 +0200

    c11

---
+++ ClavierVirtuel/Assets/ButtonTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTest : MonoBehaviour
+{
+       public AudioSource frequence1;  //création d'un objet de type audio Source
+    
+    
+
+public void C_Note_Play()
+    {
+        frequence1.Play();
+    }
+
+
+
+}
